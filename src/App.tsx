74a8ca44180d8d61ef543b5288541c96f15ddd96
Carlos Lima <carlos.lima@firedev.com>
2023-03-16 12:49:30 -0300

    remove default styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { useState } from 'react'
 import './App.css'
+import { Header } from './components/Header/Header'
 
 function App() {
-
   return (
-    <div className="App">
-      <h1>hello</h1>
+    <div>
+      <Header />
     </div>
   )
 }
