fc99c3b58d24236f04f101e270007da656e2e45b
Carlos Lima <carlos.lima@firedev.com>
2023-03-16 20:27:22 -0300

    add routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Heroes from './pages/Heroes';
+import './global.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/heroes",
+    element: <Heroes />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
