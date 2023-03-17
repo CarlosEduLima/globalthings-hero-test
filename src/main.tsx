e1ae4c741b86e5fd3333e860d29a9a2759dfbc94
Carlos Lima <carlos.lima@firedev.com>
2023-03-17 01:05:31 -0300

    add new routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import Heroes from './pages/Heroes';
-import './global.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Heroes from "./pages/Heroes";
+import "./global.css";
+import { HeroProvider } from "./contexts/Hero";
+import RegisterHero from "./pages/RegisterHero";
+import EditHero from "./pages/EditHero";
 
 const router = createBrowserRouter([
   {
@@ -14,13 +14,23 @@ const router = createBrowserRouter([
     element: <App />,
   },
   {
-    path: "/heroes",
+    path: "/herois",
     element: <Heroes />,
+  },
+  {
+    path: "/cadastro",
+    element: <RegisterHero />,
+  },
+  {
+    path: "/editar",
+    element: <EditHero />,
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+    <HeroProvider>
+      <RouterProvider router={router} />
+    </HeroProvider>
+  </React.StrictMode>
+);
