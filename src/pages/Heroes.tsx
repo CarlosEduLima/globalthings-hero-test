92593e6937637cd98b0e709ece1bb8f90a6a21ed
Carlos Lima <carlos.lima@firedev.com>
2023-03-17 01:06:44 -0300

    improve ux

--- src/pages/Heroes.tsx
+++ src/pages/Heroes.tsx
@@ -1,16 +1,31 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "./Heroes.module.css";
 import { DetailsCard } from "../components/DetailsCard/DetailsCard";
 import { Card } from "../components/Card/Card";
+import { useHero } from "../contexts/Hero";
+import { Spinner } from "phosphor-react";
 
 function Heroes() {
+  const { loadHeroes, allHeroes, selectedHero, setSelectedHero, loading } =
+    useHero();
+  useEffect(() => {
+    loadHeroes();
+  }, []);
+
   return (
-      <div className={styles.wrapper}>
-        <DetailsCard />
-        <main>
-          <Card />
-        </main>
-      </div>
+    <div className={styles.wrapper}>
+      <DetailsCard hero={selectedHero} />
+      <main>
+        {loading ? (
+          <div className={styles.loadingContainer}>
+            <span>Carregando</span>
+            <Spinner size={40} />
+          </div>
+        ) : (
+          <Card data={allHeroes} setHero={setSelectedHero} />
+        )}
+      </main>
+    </div>
   );
 }
 
