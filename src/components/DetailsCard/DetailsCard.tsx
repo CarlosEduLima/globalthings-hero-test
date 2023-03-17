d1ffb81ebdb41158eb4632f3452abfe46fe3f769
Carlos Lima <carlos.lima@firedev.com>
2023-03-17 01:06:05 -0300

    api integration

--- src/components/DetailsCard/DetailsCard.tsx
+++ src/components/DetailsCard/DetailsCard.tsx
@@ -1,33 +1,59 @@
 import styles from "./DetailsCard.module.css";
-import { PencilLine, TrashSimple, Plus } from "phosphor-react";
+import { PencilLine, TrashSimple, Plus, Spinner } from "phosphor-react";
 import { Link } from "react-router-dom";
+import { useHero } from "../../contexts/Hero";
 
-export function DetailsCard() {
+export function DetailsCard({ hero }: any) {
+  const { removeHero, loading } = useHero();
   return (
     <aside className={styles.sidebar}>
       <div className={styles.profile}>
-        <strong>Carlos</strong>
-        <span>Web developer</span>
-        <div className={styles.buttonContainer}>
-          <Link to={"heroes"} style={{ textDecoration: "none" }}>
-            <span className={styles.buttonText}>
-              <PencilLine />
-              Editar
-            </span>
-          </Link>
-        </div>
-        <div className={styles.buttonContainer}>
-          <span className={styles.buttonText}>
-            <TrashSimple />
-            Excluir
-          </span>
-        </div>
+        {hero ? (
+          <>
+            <strong>{hero?.Name}</strong>
+            <span>{hero?.Active ? "Ativo" : "Não ativo"}</span>
+            <div className={styles.buttonContainer}>
+              <Link
+                to={"/editar"}
+                state={{
+                  id: hero?.Id,
+                  name: hero?.Name,
+                  active: hero?.Active,
+                  category: hero?.Category?.Id,
+                }}
+                style={{ textDecoration: "none" }}
+              >
+                <span className={styles.buttonText}>
+                  <PencilLine />
+                  Editar
+                </span>
+              </Link>
+            </div>
+            <div
+              className={styles.buttonContainer}
+              onClick={() => removeHero(hero?.Id)}
+            >
+              {loading ? (
+                <Spinner />
+              ) : (
+                <span className={styles.buttonText}>
+                  <TrashSimple />
+                  Excluir
+                </span>
+              )}
+            </div>
+          </>
+        ) : (
+          <span>Nenhum herói selecionado</span>
+        )}
       </div>
       <div className={styles.buttonContainer}>
-        <span className={styles.buttonText}>
-          <Plus />
-          Cadastrar herói
-        </span>
+        <Link to={"/cadastro"} style={{ textDecoration: "none" }}>
+          <span className={styles.buttonText}>
+            <Plus />
+            Cadastrar herói
+          </span>
+        </Link>
       </div>
     </aside>
   );
